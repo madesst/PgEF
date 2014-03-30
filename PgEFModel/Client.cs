6f709106d70172967da7088e5b53cc90fd92c78f
madesst <madesst@gmail.com>
2014-03-30 19:45:53 +0400

    Wow wow wow

---
+++ PgEFModel/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devart.Data.Linq.Mapping;
+
+namespace PgEFModel
+{
+    public partial class Client
+    {
+        public override string ToString()
+        {
+            return Id + " | " + Email;
+        }
+    }
+}
